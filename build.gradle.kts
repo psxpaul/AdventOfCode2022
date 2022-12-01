97b03dc5befdeca125ecf8785285288d0bb12951
Paul Roberts <psxpaul@gmail.com>
2022-12-01 11:57:50 -0800

    AdventOfCode 2022 day 1

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+    application
+}
+
+group = "com.github.psxpaul"
+version = "2022-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
+dependencies {
+    implementation(kotlin("script-runtime"))
+}
