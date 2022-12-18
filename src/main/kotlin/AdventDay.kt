0fe99b63b9e2c7267b131987a5cd99e33b6f5538
Paul Roberts <psxpaul@gmail.com>
2022-12-18 00:35:30 -0800

    AdventOfCode 2022 day 18

--- src/main/kotlin/AdventDay.kt
+++ src/main/kotlin/AdventDay.kt
@@ -17,5 +17,5 @@ sealed class AdventDay {
 }
 
 fun main() {
-    DaySeventeen().run(false)
+    DayEighteeen().run(false)
 }
---
+++ src/main/kotlin/DayEighteeen.kt
@@ -0,0 +1,67 @@
+class DayEighteeen: AdventDay() {
+    override fun partOne(data: List<String>) {
+        val grid = parse(data)
+        val result = grid.map { p ->
+            var sides = 6
+            if (grid.contains(Point(p.x - 1, p.y, p.z))) sides--
+            if (grid.contains(Point(p.x + 1, p.y, p.z))) sides--
+            if (grid.contains(Point(p.x, p.y - 1, p.z))) sides--
+            if (grid.contains(Point(p.x, p.y + 1, p.z))) sides--
+            if (grid.contains(Point(p.x, p.y, p.z - 1))) sides--
+            if (grid.contains(Point(p.x, p.y, p.z + 1))) sides--
+            sides
+        }
+        println("Part 1 - ${result.sum()}")
+    }
+
+    override fun partTwo(data: List<String>) {
+        val grid = parse(data).associateWith {
+            1
+        }.toMutableMap()
+        fill(grid)
+        val result = grid.map { (p, v) ->
+            if (v == 0) return@map 0
+            var sides = 0
+            if (grid[Point(p.x - 1, p.y, p.z)] == 0) sides++
+            if (grid[Point(p.x + 1, p.y, p.z)] == 0) sides++
+            if (grid[Point(p.x, p.y - 1, p.z)] == 0) sides++
+            if (grid[Point(p.x, p.y + 1, p.z)] == 0) sides++
+            if (grid[Point(p.x, p.y, p.z - 1)] == 0) sides++
+            if (grid[Point(p.x, p.y, p.z + 1)] == 0) sides++
+            sides
+        }
+
+        // TODO - this gives the correct answer for the test data, but incorrect for the real data (2041)
+        println("Part 2 - ${result.sum()}")
+    }
+
+    private fun fill(grid: MutableMap<Point, Int>) {
+        val queue = mutableListOf(Point(0, 0, 0))
+        val maxX = grid.keys.maxOfOrNull { it.x }!! + 1
+        val maxY = grid.keys.maxOfOrNull { it.y }!! + 1
+        val maxZ = grid.keys.maxOfOrNull { it.z }!! + 1
+
+        while (queue.isNotEmpty()) {
+            val p = queue.removeFirst()
+
+            if (grid.contains(p) || p.x < 0 || p.x > maxX || p.y < 0 || p.y > maxY || p.z < 0 || p.z > maxZ) continue
+
+            grid[p] = 0
+            queue.add(Point(p.x + 1, p.y, p.z))
+            queue.add(Point(p.x - 1, p.y, p.z))
+            queue.add(Point(p.x, p.y + 1, p.z))
+            queue.add(Point(p.x, p.y - 1, p.z))
+            queue.add(Point(p.x, p.y, p.z + 1))
+            queue.add(Point(p.x, p.y, p.z - 1))
+        }
+    }
+
+    private fun parse(data: List<String>) = data.map { s -> s.split(",").map { it.toInt() }.let { (x, y, z) -> Point(x, y, z) } }.toSet()
+    data class Point(var x: Int, var y: Int, var z: Int)
+
+    enum class ThreeDeeState {
+        GAS,
+        SOLID,
+        UNKNOWN,
+    }
+}
